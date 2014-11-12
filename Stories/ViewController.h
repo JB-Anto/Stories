722ea7e89a18838b7713211c048a126373a096e9
Jean-Baptiste PENRATH <jb.penrath@gmail.com>
2014-11-12 16:22:01 +0100

    commit caca

--- Stories/ViewController.h
+++ Stories/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UICollectionView *ChapterCollectionView;
+@property (weak, nonatomic) NSArray *chapterArray;
 
 @end
 

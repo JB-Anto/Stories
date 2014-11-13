c9252353163d91715b65a3ea9edf121eabeda4a7
Jean-Baptiste PENRATH <Hi4S@MacBook-Pro-de-Jean-baptiste.local>
2014-11-13 08:18:54 +0100

    A hope

---
+++ Stories/JAChapterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JAChapterTableViewController.h
+//  Stories
+//
+//  Created by Jean-baptiste PENRATH on 13/11/2014.
+//  Copyright (c) 2014 Jb & Anto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAChapterTableViewController : UITableViewController
+
+@end
